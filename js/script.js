a0dd859c099164d8650fafe0c089b2aaed40aba3
notey08 <nickotey813@gmail.com>
2023-12-06 20:19:22 -0700

    Added comments to each object.
    I added comments to each object describing that it is an individual pokemon/object.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let pokemonList = [
-    {name: 'bulbasaur', height: 4, type: ['grass', 'poison']},
-    {name: 'charmander', height: 6, type: ['fire']},
-    {name: 'butterfree', height: 1, type: ['bug', 'flying']}
+    {name: 'bulbasaur', height: 4, type: ['grass', 'poison']}, //POKEMON/OBJECT1
+    {name: 'charmander', height: 6, type: ['fire']}, //POKEMON/OBJECT2
+    {name: 'butterfree', height: 1, type: ['bug', 'flying']} //POKEMON/OBJECT3
 ];
+
