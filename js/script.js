2f8db4dcf0e3afe0c4626a589f6dda67b60314e5
notey08 <nickotey813@gmail.com>
2023-12-06 20:17:19 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert ('Hello World');
-
-let favoriteFood = 'gyro';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'bulbasaur', height: 4, type: ['grass', 'poison']},
+    {name: 'charmander', height: 6, type: ['fire']},
+    {name: 'butterfree', height: 1, type: ['bug', 'flying']}
+];
