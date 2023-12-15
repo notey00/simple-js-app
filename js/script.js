71cc87fbede9993c69380f12100583ec36764f53
notey08 <nickotey813@gmail.com>
2023-12-14 21:39:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ let pokemonList = [
     {name: 'butterfree', height: 1, type: ['bug', 'flying']} //POKEMON/OBJECT3
 ];
 
+// for (i=0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "   ");
+// }
+
+for (i=0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 4) {
+        document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + " - Thats a big Poekmon! "); //THIS PART OF THE CODE RUNS THROUGH ALL THE POEKMONS AND LOOKS FOR THE ONES WITH THE HEIGHT VALUE ABOVE 4
+    }
+    else {
+      document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "   ");  //THIS IS THE MESSAGE THAT IS GENERATED WHEN THE HEIGHT IS LESS THAN 4
+    }
+    
+}
+
