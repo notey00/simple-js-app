0bdb5428d2640ed0e8b97048b9fa14a22ba2b77d
notey00 <41129706+notey00@users.noreply.github.com>
2023-12-28 22:21:48 -0700

    Updated my script.js with IIFE guidelines and added an object and printed out the new array
    Changed my current array(pokemonList) and wrapped it in a IIFE and called it pokemonRepository and also added a new pokemon to my array with the add function and push method. Also, using the getAll method I got the new array to print out onto the screen.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
-let pokemonList = [
-    {name: 'bulbasaur', height: 4, type: ['grass', 'poison']}, //POKEMON/OBJECT1
-    {name: 'charmander', height: 6, type: ['fire']}, //POKEMON/OBJECT2
-    {name: 'butterfree', height: 1, type: ['bug', 'flying']} //POKEMON/OBJECT3
-];
+let pokemonRepository = (function (){
+    let pokemonList = [
+    {name: 'Bulbasaur', height: 4, type: ['grass', 'poison']}, //POKEMON/OBJECT1
+    {name: 'Charmander', height: 6, type: ['fire']}, //POKEMON/OBJECT2
+    {name: 'Butterfree', height: 1, type: ['bug', 'flying']} //POKEMON/OBJECT3
+]
+
+    return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+
+}());
+
+
 
 // for (i=0; i < pokemonList.length; i++) {
 //     document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "   ");
 // }
 
-for (i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 4) {
-        document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + " - Thats a big Poekmon! "); //THIS PART OF THE CODE RUNS THROUGH ALL THE POEKMONS AND LOOKS FOR THE ONES WITH THE HEIGHT VALUE ABOVE 4
-    }
-    else {
-      document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "   ");  //THIS IS THE MESSAGE THAT IS GENERATED WHEN THE HEIGHT IS LESS THAN 4
-    }
+// for (i=0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height > 4) {
+//         document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + " - Thats a big Poekmon! "); //THIS PART OF THE CODE RUNS THROUGH ALL THE POEKMONS AND LOOKS FOR THE ONES WITH THE HEIGHT VALUE ABOVE 4
+//     }
+//     else {
+//       document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "   ");  //THIS IS THE MESSAGE THAT IS GENERATED WHEN THE HEIGHT IS LESS THAN 4
+//     }
     
-}
+// }
 
+// pokemonList.forEach(function(pokemon) {
+//     document.write(pokemon.name + ' ' + pokemon.height + ' ' + pokemon.type + "<br>")
+// });
+
+pokemonRepository.add({name:'Mew', height: 2, type:'psychic'});
+// pokemonRepository.getAll();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + ':' + ' ' + 'height: ' + pokemon.height + ' & ' + 'type: ' + pokemon.type + "<br>")
+});
